af85f2447769c102d3e89a0c87a48ee62b2255dc
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-10-27 17:29:36 -0600

    reorganized main controller and view classes

--- src/main/java/wgu/software2project/Main.java
+++ src/main/java/wgu/software2project/Main.java
@@ -2,22 +2,25 @@ package wgu.software2project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainView.fxml")));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.sizeToScene();
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
---
+++ src/main/java/wgu/software2project/MainController.java
@@ -0,0 +1,9 @@
+package wgu.software2project;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+        
+    }
+}
