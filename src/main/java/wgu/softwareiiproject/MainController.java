d7d56b7efd95ba90eae87713bfbbb13d1d682ff1
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-11-01 20:08:23 -0600

    added customer class for later use

--- src/main/java/wgu/softwareiiproject/MainController.java
+++ src/main/java/wgu/softwareiiproject/MainController.java
@@ -1,16 +1,32 @@
 package wgu.softwareiiproject;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class MainController {
+
+    @FXML
+    private TableColumn<Customer, Integer> customerId;
+    @FXML
+    private TableColumn<Customer, String>  customerName;
+    @FXML
+    private TableColumn<Customer, String>  customerAddress;
+    @FXML
+    private TableColumn<Customer, Integer>  customerZipCode;
+    @FXML
+    private TableColumn<Customer, Integer>  divisionId;
+    @FXML
+    private TableColumn<Customer, Integer>  customerPhoneNumber;
+
     public void clickLogOut(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginView.fxml")));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
