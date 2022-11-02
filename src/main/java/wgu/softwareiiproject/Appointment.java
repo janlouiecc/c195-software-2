72c3ffd2bbe5881a52d7358101a78510f6a41fa5
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-11-01 20:27:29 -0600

    completed appointment table + appointment class add

---
+++ src/main/java/wgu/softwareiiproject/Appointment.java
@@ -0,0 +1,4 @@
+package wgu.softwareiiproject;
+
+public class Appointment {
+}
--- src/main/java/wgu/softwareiiproject/MainController.java
+++ src/main/java/wgu/softwareiiproject/MainController.java
@@ -8,8 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.Date;
 import java.util.Objects;
 
 public class MainController {
@@ -26,6 +26,26 @@ public class MainController {
     private TableColumn<Customer, Integer>  divisionId;
     @FXML
     private TableColumn<Customer, Integer>  customerPhoneNumber;
+    @FXML
+    private TableColumn<Appointment, Integer> appointmentId;
+    @FXML
+    private TableColumn<Appointment, String> appointmentTitle;
+    @FXML
+    private TableColumn<Appointment, Integer> customerId1;
+    @FXML
+    private TableColumn<Appointment, String> appointmentDescript;
+    @FXML
+    private TableColumn<Appointment, String> appointmentLocation;
+    @FXML
+    private TableColumn<Appointment, Integer> appointmentContact;
+    @FXML
+    private TableColumn<Appointment, String> appointmentType;
+    @FXML
+    private TableColumn<Appointment, Date> appointmentStart;
+    @FXML
+    private TableColumn<Appointment, Date> appointmentEnd;
+    @FXML
+    private TableColumn<Appointment, Integer> userId;
 
     public void clickLogOut(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginView.fxml")));
