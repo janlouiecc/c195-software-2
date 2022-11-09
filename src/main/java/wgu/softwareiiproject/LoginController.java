c977af7984f83387d531d74d7af7b97bc3f35735
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-11-08 20:20:40 -0700

    finished login capabilities while using database for validation

--- src/main/java/wgu/softwareiiproject/LoginController.java
+++ src/main/java/wgu/softwareiiproject/LoginController.java
@@ -1,10 +1,13 @@
 package wgu.softwareiiproject;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,16 +15,29 @@ import java.sql.SQLException;
 import java.util.Objects;
 
 public class LoginController {
+    @FXML
+    private TextField loginUserName;
+    @FXML
+    private TextField loginPw;
+
     public void clickLogin(ActionEvent event) throws IOException, SQLException {
-        if(Queries.login("rrty")) {
+        if(Queries.login(loginUserName.getText(), loginPw.getText())) {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainView.fxml")));
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setResizable(false);
             stage.show();
-        } else{
-            System.out.println("WRONG PASSWORD");
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("DENIED");
+            alert.setHeaderText("Access Denied");
+            if(loginUserName.getText().isBlank() || loginPw.getText().isBlank()) {
+                alert.setContentText("Your Username or Password is missing.");
+            } else {
+                alert.setContentText("Your Username or Password is incorrect.");
+            }
+            alert.showAndWait();
         }
     }
 }
