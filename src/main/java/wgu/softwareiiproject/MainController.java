04065aff29e87a9d88fe92d680b4fadc6313b22c
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-11-12 18:41:09 -0700

    cleanup

--- src/main/java/wgu/softwareiiproject/MainController.java
+++ src/main/java/wgu/softwareiiproject/MainController.java
@@ -3,18 +3,22 @@ package wgu.softwareiiproject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
 
     @FXML
     private TableView<Customer> mainCustomerTblView;
@@ -96,4 +100,8 @@ public class MainController {
         stage.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
