bc7bb6be0e4621cd5413df3910558bf8ad615f2d
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-11-28 16:49:43 -0700

    completed javadoc comments for TimeConversion class

--- src/main/java/wgu/softwareiiproject/TimeConversion.java
+++ src/main/java/wgu/softwareiiproject/TimeConversion.java
@@ -2,13 +2,18 @@ package wgu.softwareiiproject;
 
 import java.time.*;
 
+/**
+ * The Time Conversion class.
+ * This class holds all the methods for converting UTC time in the database.
+ */
 public class TimeConversion {
 
-//    public static LocalDateTime convertToUTC(LocalDateTime ldt) {
-//        ZonedDateTime utcZdt = ZonedDateTime.ofInstant(ZonedDateTime.of(ldt, ZoneId.systemDefault()).toInstant(), ZoneId.of("UTC"));
-//        return LocalDateTime.of(utcZdt.getYear(), utcZdt.getMonth(), utcZdt.getDayOfMonth(), utcZdt.getHour(), utcZdt.getMinute());
-//    }
-
+    /**
+     * Converts UTC to the user's local time.
+     * This method takes the UTC date time in the database and converts it to the user's system default time zone.
+     * @param ldt The local date time that is being converted.
+     * @return The local date time that has been converted to the user's system default time zone.
+     */
     public static LocalDateTime convertToLocal(LocalDateTime ldt) {
         ZonedDateTime myZdt;
         ZonedDateTime utcZdt = ZonedDateTime.ofInstant(ZonedDateTime.of(ldt, ZoneId.systemDefault()).toInstant(), ZoneId.of("UTC"));
@@ -16,6 +21,12 @@ public class TimeConversion {
         return LocalDateTime.of(myZdt.getYear(), myZdt.getMonth(), myZdt.getDayOfMonth(), myZdt.getHour(), myZdt.getMinute());
     }
 
+    /**
+     * Converts a given time to ET for business hours logic.
+     * This method takes the date time parameter and converts it to ET.
+     * @param ldt The local date time that is being converted.
+     * @return The local date time that has been converted to ET.
+     */
     public static LocalDateTime convertLocalToET(LocalDateTime ldt) {
         ZonedDateTime myZdt;
         ZonedDateTime utcZdt = ZonedDateTime.ofInstant(ZonedDateTime.of(ldt, ZoneId.systemDefault()).toInstant(), ZoneId.of("UTC"));
