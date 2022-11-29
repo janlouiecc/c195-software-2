9348cfaa64e1180bab9f283640792b3afae122e2
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-11-28 17:16:16 -0700

    completed javadoc comments for Customer class

--- src/main/java/wgu/softwareiiproject/Customer.java
+++ src/main/java/wgu/softwareiiproject/Customer.java
@@ -3,6 +3,9 @@ package wgu.softwareiiproject;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * The Customer class.
+ */
 public class Customer {
 
     private int customerId;
@@ -14,6 +17,16 @@ public class Customer {
     protected final static ObservableList<Customer> customerData = FXCollections.observableArrayList();
     static int customerCount = 0;
 
+    /**
+     * Instantiates a new customer.
+     * The constructor for the customer class.
+     * @param customerId The customer's ID which is auto generated
+     * @param customerName The customer's name
+     * @param customerAddress The customer's address
+     * @param customerPostalCode The customer's postal code
+     * @param customerPhoneNumber The customer's phone number
+     * @param customerDivisionName The customer's division name
+     */
     public Customer(int customerId,
                     String customerName,
                     String customerAddress,
@@ -29,63 +42,111 @@ public class Customer {
         customerCount++;
     }
 
+    /**
+     * Gets customer ID.
+     * The getter for the customer ID field.
+     * @return The customer ID
+     */
     public int getCustomerId() {
         return customerId;
     }
 
+    /**
+     * Sets the customer ID.
+     * The setter for the customer ID
+     * @param customerId The customer ID
+     */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
+    /**
+     * Gets customer name.
+     * The getter for the customer name field.
+     * @return The customer name
+     */
     public String getCustomerName() {
         return customerName;
     }
 
+    /**
+     * Sets the customer name.
+     * The setter for the customer name
+     * @param customerName The customer name
+     */
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
 
+    /**
+     * Gets customer address.
+     * The getter for the customer address field.
+     * @return The customer address
+     */
     public String getCustomerAddress() {
         return customerAddress;
     }
 
+    /**
+     * Sets the customer address.
+     * The setter for the customer address
+     * @param customerAddress The customer address
+     */
     public void setCustomerAddress(String customerAddress) {
         this.customerAddress = customerAddress;
     }
 
+    /**
+     * Gets customer postal code.
+     * The getter for the customer postal code field.
+     * @return The customer postal code
+     */
     public String getCustomerPostalCode() {
         return customerPostalCode;
     }
 
+    /**
+     * Sets the customer postal code.
+     * The setter for the customer postal code
+     * @param customerPostalCode The customer postal code
+     */
     public void setCustomerPostalCode(String customerPostalCode) {
         this.customerPostalCode = customerPostalCode;
     }
 
+    /**
+     * Gets customer phone number.
+     * The getter for the customer phone number field.
+     * @return The customer phone number
+     */
     public String getCustomerPhoneNumber() {
         return customerPhoneNumber;
     }
 
+    /**
+     * Sets the customer phone number.
+     * The setter for the customer phone number
+     * @param customerPhoneNumber The customer phone number
+     */
     public void setCustomerPhoneNumber(String customerPhoneNumber) {
         this.customerPhoneNumber = customerPhoneNumber;
     }
 
+    /**
+     * Gets customer division name.
+     * The getter for the customer division name field.
+     * @return The customer division name
+     */
     public String getCustomerDivisionName() {
         return customerDivisionName;
     }
 
+    /**
+     * Sets the customer division name.
+     * The setter for the customer division name
+     * @param customerDivisionName The customer division name
+     */
     public void setCustomerDivisionName(String customerDivisionName) {
         this.customerDivisionName = customerDivisionName;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", customerName='" + customerName + '\'' +
-                ", customerAddress='" + customerAddress + '\'' +
-                ", customerPostalCode='" + customerPostalCode + '\'' +
-                ", customerPhoneNumber='" + customerPhoneNumber + '\'' +
-                ", customerDivisionId=" + customerDivisionName +
-                '}';
     }
 }
